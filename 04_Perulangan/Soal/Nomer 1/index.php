792f21a770f924a2f1a1095036a601b80c240002
adhamnugroho <76804759+adhamnugroho@users.noreply.github.com>
2021-10-09 06:25:50 +0700

    Manambahkan repo perulangan

---
+++ 04_Perulangan/Soal/Nomer 1/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Soal Nomer 1 Adham P.S.N.</title>
+</head>
+<body>
+	
+	<?php
+
+		// Deklarasi + Assigment variabel
+		$n = 5;
+
+
+		// Perulangan
+		for ($i = 0; $i <= $n; $i++) { 
+			for ($j = 0; $j < $i; $j++) { 
+				echo "* ";
+			}
+
+			echo "<br>";
+		}
+
+	?>
+
+</body>
+</html>
---
+++ 04_Perulangan/Soal/Nomer 2/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Soal nomer 2 Adham P.S.N.</title>
+</head>
+<body>
+	
+	<?php
+
+		// Deklarasi + Assigment variabel
+		$n = 6;
+
+
+		// perulangan
+		for ($i = 1; $i < $n ; $i++){
+			echo "$i + ";
+		}
+
+
+
+		// percabangan
+		if ($i == $n) {
+				echo "$i";
+
+				$i *= 3;
+				$i += 3;
+
+				echo " = $i";
+		}
+	?>
+
+</body>
+</html>
